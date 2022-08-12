be851b633ea679f3902fa89ebf6cb95df6bf6a29
Arnyfavy <daveside00468@gmail.com>
2022-08-12 01:34:10 -0700

    Test

--- functions1.c
+++ functions1.c
@@ -59,9 +59,9 @@ int print_octal(va_list types, char buffer[],
 	num = convert_size_unsgnd(num, size);
 
 	if (num == 0)
-	buffer[i--] = '0';
+		buffer[i--] = '0';
 
-		buffer[BUFF_SIZE - 1] = '\0';
+	buffer[BUFF_SIZE - 1] = '\0';
 
 	while (num > 0)
 	{
@@ -92,7 +92,7 @@ int print_hexadecimal(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
 {
 	return (print_hexa(types, "0123456789abcdef", buffer,
-									flags, 'x', width, precision, size));
+				flags, 'x', width, precision, size));
 }
 
 /************* PRINT UNSIGNED NUMBER IN UPPER HEXADECIMAL **************/
@@ -110,7 +110,7 @@ int print_hexa_upper(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
 {
 	return (print_hexa(types, "0123456789ABCDEF", buffer,
-									flags, 'X', width, precision, size));
+				flags, 'X', width, precision, size));
 }
 
 /************** PRINT HEXX NUM IN LOWER OR UPPER **************/
